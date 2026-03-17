f4ac638ee8f2cefaa247cf5ef07f2cae346e4735
letuanvux <100011860+letuanvux@users.noreply.github.com>
2026-03-17 12:09:16 +0700

    Add files via upload

---
+++ lib/features/scan/scan_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ScanScreen extends StatelessWidget {
+  const ScanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Scan Tag")),
+      body: MobileScanner(
+        onDetect: (barcode, args) {
+          final code = barcode.rawValue;
+          debugPrint("Scanned: $code");
+        },
+      ),
+    );
+  }
+}
---
+++ lib/routes/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../features/home/home_screen.dart';
+import '../features/pet/pet_detail_screen.dart';
+import '../features/scan/scan_screen.dart';
+
+class AppRouter {
+  static Route generate(RouteSettings settings) {
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case "/pet":
+        return MaterialPageRoute(builder: (_) => const PetDetailScreen());
+      case "/scan":
+        return MaterialPageRoute(builder: (_) => const ScanScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+    }
+  }
+}
