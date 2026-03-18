6c350c18aace3c48a1f8c41b4cd5b70e57757309
letuanvux <100011860+letuanvux@users.noreply.github.com>
2026-03-18 11:19:57 +0700

    Refactor PetTagApp by removing AppRouter
    Removed AppRouter dependency and simplified main app structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-import 'routes/app_router.dart';
+import 'app.dart';
 
 void main() {
   runApp(const PetTagApp());
-}
-
-class PetTagApp extends StatelessWidget {
-  const PetTagApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Pet Tag",
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
-      onGenerateRoute: AppRouter.generate,
-      initialRoute: "/",
-    );
-  }
 }
