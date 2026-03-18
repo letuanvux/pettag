26194e9f6a57b971487322aba7aca5819b887eb6
letuanvux <100011860+letuanvux@users.noreply.github.com>
2026-03-18 11:02:55 +0700

    Add files via upload

---
+++ lib/models/pet.dart
@@ -0,0 +1,17 @@
+class Pet {
+
+  final String name;
+  final String breed;
+  final int age;
+  final double weight;
+  final String image;
+
+  Pet({
+    required this.name,
+    required this.breed,
+    required this.age,
+    required this.weight,
+    required this.image,
+  });
+
+}
